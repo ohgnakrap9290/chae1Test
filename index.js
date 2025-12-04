fcbef02ce48eb2535bc1c354e3a35dd555a7d458
k@NGh5! <phantom4862@naver.com>
2025-12-05 02:20:51 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+setInterval(() => {
+    let imgs = document.querySelectorAll('img');
+    imgs.forEach((a, i) => {
+        a.src =
+            'https://ichef.bbci.co.uk/ace/ws/640/cpsprodpb/16620/production/_91408619_55df76d5-2245-41c1-8031-07a4da3f313f.jpg.webp';
+    });
+}, 500);
